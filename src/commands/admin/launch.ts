76371f3e85d2a63df158deabab85cc6086436228
Tom Chappaz <tom.chappaz1@gmail.com>
2025-07-20 23:45:20 +0200

    feat(core): queue skeleton

--- src/commands/admin/launch.ts
+++ src/commands/admin/launch.ts
@@ -1,10 +1,10 @@
-import { CommandInteraction, MessageFlags, SlashCommandBuilder } from 'discord.js';
+import { ActionRowBuilder, ButtonBuilder, ButtonStyle, CommandInteraction, ComponentType, MessageFlags, SlashCommandBuilder, TextChannel } from 'discord.js';
 import { botModGuard } from '../../guards/bot-mod.guard';
 
 export const data = new SlashCommandBuilder().setName('launch').setDescription('Starts the queue');
 
 export async function execute(interaction: CommandInteraction) {
-    botModGuard(interaction, (guild) => {
+    botModGuard(interaction, async (guild) => {
         const queueChannel = interaction.client.channels.cache.get(guild.queueChannel!);
 
         if (!queueChannel) {
@@ -12,8 +12,28 @@ export async function execute(interaction: CommandInteraction) {
             return;
         }
 
-        if (queueChannel.isSendable()) {
-            queueChannel.send('Hello :)'); // TODO: queue embed + buttons etc. etc.
+        if (queueChannel instanceof TextChannel) {
+            await queueChannel.bulkDelete(100);
+
+            const queueButton = new ButtonBuilder().setCustomId('queueButton').setLabel('Queue').setStyle(ButtonStyle.Primary);
+            const leaveButton = new ButtonBuilder().setCustomId('leaveButton').setLabel('Leave').setStyle(ButtonStyle.Danger);
+            const queueButtonsRow = new ActionRowBuilder<ButtonBuilder>().addComponents(queueButton).addComponents(leaveButton);
+
+            const queueMessage = await queueChannel.send({ content: 'Good luck!', components: [queueButtonsRow] });
+
+            const buttonCollector = queueMessage.createMessageComponentCollector({ componentType: ComponentType.Button });
+
+            buttonCollector?.on('collect', async (i) => {
+                switch (i.customId) {
+                    case 'queueButton':
+                        await i.reply({ content: 'Queue joined!', flags: MessageFlags.Ephemeral });
+                        break;
+                    case 'leaveButton':
+                        await i.reply({ content: 'Queue left!', flags: MessageFlags.Ephemeral });
+                        break;
+                }
+            });
+
             interaction.reply({ content: 'Queue has been launched!', flags: MessageFlags.Ephemeral });
         } else {
             interaction.reply({ content: 'Cannot send messages to queue channel, check bot permissions!', flags: MessageFlags.Ephemeral });
